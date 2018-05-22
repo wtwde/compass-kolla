aee4225fe7a43649c5c944bac6392d2f16ff09d9
wutianwei <wutianwei1@huawei.com>
2018-05-22 20:26:27 +0800

    create cluster and deploy kolla

---
+++ ansible_plugins/callback/status_callback.py
@@ -0,0 +1,203 @@
+##############################################################################
+# Copyright (c) 2016 HUAWEI TECHNOLOGIES CO.,LTD and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+import httplib
+import simplejson as json
+import sys  # noqa:F401
+
+from distutils.version import LooseVersion
+from ansible import __version__ as __ansible_version__
+from ansible.plugins.callback import CallbackBase
+
+COMPASS_HOST = "compass-deck"
+
+
+# def task_error(display, host, data):
+#     display.display("task_error: host=%s,data=%s" % (host, data))
+#
+#     if isinstance(data, dict):
+#         invocation = data.pop('invocation', {})
+#
+#     notify_host(display, COMPASS_HOST, host, "failed")
+
+
+class CallbackModule(CallbackBase):
+    """
+    logs playbook results, per host, in /var/log/ansible/hosts
+    """
+    CALLBACK_VERSION = 2.0
+    CALLBACK_TYPE = 'notification'
+    CALLBACK_NAME = 'status_callback'
+    CALLBACK_NEEDS_WHITELIST = True
+
+    def __init__(self):
+        super(CallbackModule, self).__init__()
+
+    def v2_on_any(self, *args, **kwargs):
+        pass
+
+    def v2_runner_on_failed(self, res, ignore_errors=False):
+        # task_error(self._display, host, res)
+        pass
+
+    def v2_runner_on_ok(self, res):
+        pass
+
+    def v2_runner_on_skipped(self, host, item=None):
+        pass
+
+    def v2_runner_on_unreachable(self, host, res):
+        pass
+
+    def v2_runner_on_no_hosts(self):
+        pass
+
+    def v2_runner_on_async_poll(self, host, res, jid, clock):
+        pass
+
+    def v2_runner_on_async_ok(self, host, res, jid):
+        pass
+
+    def v2_runner_on_async_failed(self, host, res, jid):
+        # task_error(self._display, host, res)
+        pass
+
+    def v2_playbook_on_start(self):
+        pass
+
+    def v2_playbook_on_notify(self, host, handler):
+        pass
+
+    def v2_playbook_on_no_hosts_matched(self):
+        pass
+
+    def v2_playbook_on_no_hosts_remaining(self):
+        pass
+
+    def v2_playbook_on_task_start(self, name, is_conditional):
+        pass
+
+    def v2_playbook_on_vars_prompt(self, varname, private=True, prompt=None,
+                                encrypt=None, confirm=False, salt_size=None, salt=None, default=None):   # noqa
+        pass
+
+    def v2_playbook_on_setup(self):
+        pass
+
+    def v2_playbook_on_import_for_host(self, host, imported_file):
+        pass
+
+    def v2_playbook_on_not_import_for_host(self, host, missing_file):
+        pass
+
+    def v2_playbook_on_play_start(self, play):
+        self.play = play
+        self.loader = self.play.get_loader()
+        return
+
+    def v2_playbook_on_stats(self, stats):
+        self._display.display("playbook_on_stats enter")
+        if LooseVersion(__ansible_version__) < LooseVersion("2.4"):
+            all_vars = self.play.get_variable_manager().get_vars(self.loader)
+        else:
+            all_vars = self.play.get_variable_manager().get_vars()
+        host_vars = all_vars["hostvars"]
+        hosts = sorted(stats.processed.keys())
+        cluster_name = host_vars[hosts[0]]['cluster_name']
+
+        headers = {"Content-type": "application/json",
+                   "Accept": "*/*"}
+        conn = httplib.HTTPConnection(COMPASS_HOST, 80)
+        token = auth(conn)
+        headers["X-Auth-Token"] = token
+        get_url = "/api/clusterhosts"
+        conn.request("GET", get_url, "", headers)
+        resp = conn.getresponse()
+        raise_for_status(resp)
+        clusterhost_data = json.loads(resp.read())
+        clusterhost_mapping = {}
+        for item in clusterhost_data:
+            if item["clustername"] == cluster_name:
+                clusterhost_mapping.update({item["hostname"]:
+                                           item["clusterhost_id"]})
+
+        force_error = False
+        if "localhost" in hosts:
+            summary = stats.summarize("localhost")
+            if summary['failures'] > 0 or summary['unreachable'] > 0:
+                force_error = True
+
+        for hostname, hostid in clusterhost_mapping.iteritems():
+            if hostname not in hosts:
+                continue
+
+            summary = stats.summarize(hostname)
+            # self._display.display("host: %s \nsummary: %s\n" % (host, summary)) # noqa
+
+            if summary['failures'] > 0 or summary['unreachable'] > 0 \
+               or force_error:
+                status = "error"
+            else:
+                status = "succ"
+            self._display.display("hostname: %s" % hostname)
+            notify_host(self._display, COMPASS_HOST, hostid, status)
+
+
+def raise_for_status(resp):
+    if resp.status < 200 or resp.status > 300:
+        raise RuntimeError(
+            "%s, %s, %s" %
+            (resp.status, resp.reason, resp.read()))
+
+
+def auth(conn):
+    credential = {}
+    credential['email'] = "admin@huawei.com"
+    credential['password'] = "admin"
+    url = "/api/users/token"
+    headers = {"Content-type": "application/json",
+               "Accept": "*/*"}
+    conn.request("POST", url, json.dumps(credential), headers)
+    resp = conn.getresponse()
+
+    raise_for_status(resp)
+    return json.loads(resp.read())["token"]
+
+
+def notify_host(display, compass_host, hostid, status):
+    url = "/api/clusterhosts/%s/state" % hostid
+    if status == "succ":
+        body = {"state": "SUCCESSFUL"}
+    elif status == "error":
+        body = {"state": "ERROR"}
+    else:
+        display.error("notify_host: hostid %s with status %s is not supported"
+                      % (hostid, status))
+        return
+
+    headers = {"Content-type": "application/json",
+               "Accept": "*/*"}
+
+    conn = httplib.HTTPConnection(compass_host, 80)
+    token = auth(conn)
+    headers["X-Auth-Token"] = token
+    display.display("host=%s,url=%s,body=%s,headers=%s" %
+                    (compass_host, url, json.dumps(body), headers))
+    conn.request("POST", url, json.dumps(body), headers)
+    resp = conn.getresponse()
+    try:
+        raise_for_status(resp)
+        display.display(
+            "notify host status success!!! status=%s, body=%s" %
+            (resp.status, resp.read()))
+    except Exception as e:
+        display.error("http request failed %s" % str(e))
+        raise
+    finally:
+        conn.close()
---
+++ config_parse.py
@@ -0,0 +1,168 @@
+##############################################################################
+# Copyright (c) 2016 HUAWEI TECHNOLOGIES CO.,LTD and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+import os
+import netaddr
+import yaml
+import sys
+from Cheetah.Template import Template
+
+
+def init(file):
+    with open(file) as fd:
+        return yaml.safe_load(fd)
+
+
+def export_env_dict(env_dict, ofile, direct=False):
+    if not os.path.exists(ofile):
+        raise IOError("output file: %s not exist" % ofile)
+    if direct:
+        for k, v in env_dict.items():
+            os.system("echo 'export %s=\"%s\"' >> %s" % (k, v, ofile))
+    else:
+        for k, v in env_dict.items():
+            os.system("echo 'export %s=${%s:-%s}' >> %s" % (k, k, v, ofile))
+
+
+def decorator(func):
+    def wrapter(s, seq):
+        host_list = s.get('hosts', [])
+        result = []
+        for host in host_list:
+            s = func(s, seq, host)
+            if not s:
+                continue
+            result.append(s)
+        if len(result) == 0:
+            return ""
+        else:
+            return "\"" + seq.join(result) + "\""
+    return wrapter
+
+
+@decorator
+def hostnames(s, seq, host=None):
+    return host.get('name', '')
+
+
+@decorator
+def hostroles(s, seq, host=None):
+    return "%s=%s" % (host.get('name', ''), ','.join(host.get('roles', [])))
+
+
+@decorator
+def hostmacs(s, seq, host=None):
+    return host.get('mac', '')
+
+
+def export_network_file(dha, network, ofile):
+    install_network_env = {}
+    host_network_env = {}
+    ip_settings = network['ip_settings']
+    mgmt_net = [item for item in ip_settings
+                if item['name'] == 'mgmt'][0]
+    mgmt_gw = mgmt_net['gw']
+    mgmt_cidr = mgmt_net['cidr']
+    prefix = int(mgmt_cidr.split('/')[1])
+    mgmt_netmask = '.'.join([str((0xffffffff << (32 - prefix) >> i) & 0xff)
+                             for i in [24, 16, 8, 0]])
+    dhcp_ip_range = ' '.join(mgmt_net['dhcp_ranges'][0])
+    internal_vip = network['internal_vip']['ip']
+    install_network_env.update({'INSTALL_GW': mgmt_gw})
+    install_network_env.update({'INSTALL_CIDR': mgmt_cidr})
+    install_network_env.update({'INSTALL_NETMASK': mgmt_netmask})
+    install_network_env.update({'INSTALL_IP_RANGE': dhcp_ip_range})
+    install_network_env.update({'VIP': internal_vip})
+    export_env_dict(install_network_env, ofile)
+
+    pxe_nic = os.environ['PXE_NIC']
+    host_ip_range = mgmt_net['ip_ranges'][0]
+    host_ips = netaddr.iter_iprange(host_ip_range[0], host_ip_range[1])
+    host_networks = []
+    for host in dha['hosts']:
+        host_name = host['name']
+        host_ip = str(host_ips.next())
+        host_networks.append(
+            '{0}:{1}={2}|is_mgmt'.format(host_name, pxe_nic, host_ip))
+    host_subnets = [item['cidr'] for item in ip_settings]
+    host_network_env.update({'NETWORK_MAPPING': "install=" + pxe_nic})
+    host_network_env.update({'HOST_NETWORKS': ';'.join(host_networks)})
+    host_network_env.update({'SUBNETS': ','.join(host_subnets)})
+    export_env_dict(host_network_env, ofile, True)
+
+
+def export_dha_file(dha, dha_file, ofile):
+    env = {}
+    env.update(dha)
+    if env.get('hosts', []):
+        env.pop('hosts')
+    if 'plugins' in env:
+        plugin_list = []
+        for item in env.get('plugins'):
+            plugin_str = ':'.join([item.keys()[0], item.values()[0]])
+            plugin_list.append(plugin_str)
+        env.update({'plugins': ','.join(plugin_list)})
+
+    env.update({'CLUSTER_NAME': dha.get('NAME', "opnfv")})
+    env.update({'TYPE': dha.get('TYPE', "virtual")})
+    env.update({'FLAVOR': dha.get('FLAVOR', "cluster")})
+    env.update({'HOSTNAMES': hostnames(dha, ',')})
+    env.update({'HOST_ROLES': hostroles(dha, ';')})
+    env.update({'DHA': dha_file})
+
+    value = hostmacs(dha, ',')
+    if len(value) > 0:
+        env.update({'HOST_MACS': value})
+
+    export_env_dict(env, ofile)
+
+
+def export_reset_file(dha, tmpl_dir, output_dir, ofile):
+    tmpl_file_name = dha.get('POWER_TOOL', '')
+    if not tmpl_file_name:
+        return
+
+    tmpl = Template(
+        file=os.path.join(
+            tmpl_dir,
+            'power',
+            tmpl_file_name +
+            '.tmpl'),
+        searchList=dha)
+
+    reset_file_name = os.path.join(output_dir, tmpl_file_name + '.sh')
+    with open(reset_file_name, 'w') as f:
+        f.write(tmpl.respond())
+
+    power_manage_env = {'POWER_MANAGE': reset_file_name}
+    export_env_dict(power_manage_env, ofile, True)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 6:
+        print("parameter wrong%d %s" % (len(sys.argv), sys.argv))
+        sys.exit(1)
+
+    _, dha_file, network_file, tmpl_dir, output_dir, output_file = sys.argv
+
+    if not os.path.exists(dha_file):
+        print("%s is not exist" % dha_file)
+        sys.exit(1)
+
+    ofile = os.path.join(output_dir, output_file)
+    os.system("touch %s" % ofile)
+    os.system("echo \#config file deployment parameter > %s" % ofile)
+
+    dha_data = init(dha_file)
+    network_data = init(network_file)
+
+    export_dha_file(dha_data, dha_file, ofile)
+    export_network_file(dha_data, network_data, ofile)
+    export_reset_file(dha_data, tmpl_dir, output_dir, ofile)
+
+    sys.exit(0)
